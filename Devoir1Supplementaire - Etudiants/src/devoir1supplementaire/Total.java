61dc6b9d555d171d09e2982634433e0e57d591df
Eva Poney <eva.poney55@gmail.com>
2020-11-17 11:23:23 +0100

    Controle

---
+++ Devoir1Supplementaire - Etudiants/src/devoir1supplementaire/Total.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package devoir1supplementaire;
+
+/**
+ *
+ * @author sio1
+ */
+class Total<T0, T1> {
+    
+}
